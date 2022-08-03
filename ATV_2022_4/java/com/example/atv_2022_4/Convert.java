2ea9c4362a7aec90f7955f84827f1bdc9b4a3ea0
Hasad Alejandro <69811596+Hasad-Alejandro@users.noreply.github.com>
2022-08-03 12:42:00 -0500

    Add files via upload

---
+++ ATV_2022_4/java/com/example/atv_2022_4/Convert.java
@@ -0,0 +1,16 @@
+package com.example.atv_2022_4;
+
+public class Convert {
+
+    public double c2f_fn(double celsius){
+
+        return (celsius*1.8)+32;
+    }
+
+    public double f2c_fn(double farenheit){
+
+        return (farenheit-32)/1.8;
+
+
+    }
+}
